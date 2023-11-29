a2d1c730a0693c0422fd72c81443e2b3559456e5
Cristian Crișan <107143484+ccrisan7@users.noreply.github.com>
2023-11-29 22:43:44 +0200

    Update SP_tb.v

--- SP/SP_tb.v
+++ SP/SP_tb.v
@@ -1,7 +1,7 @@
 module SP_tb;
     reg pop, push, reset, clk;
     wire [15:0] val, out;
-    localparam CLOCK_CYCLES = 60, CLOCK_PERIOD = 20;
+    localparam CLOCK_CYCLES = 10, CLOCK_PERIOD = 20;
 
     SP SP_inst(
         .pop(pop),
@@ -31,66 +31,28 @@ module SP_tb;
         #20 
 
 	reset = 0;
-        push = 1;
-        #20;
-
 	pop = 1;
-        reset = 1;
-        #40 
+	push = 0;
+        #20 
 
-	reset = 0;
-        push = 0;
-        #20;
-        push = 0;
-        pop = 1;
-        #20;
-	push = 1;
+	push = 0;
         pop = 1;
         #20;
 	push = 0;
         pop = 1;
-        #20;
-	push = 1;
-        pop = 1;
-        #20;
-	push = 0;
-        pop = 1;
-	reset = 1;
-	#40;
-
-	reset = 0;
-        #20;
-	push = 1;
-        pop = 0;
-        #20;
-	push = 1;
-        pop = 0;
-        #20;
-	push = 1;
-        pop = 0;
-        #20;
-	push = 1;
-        pop = 0;
-        #20;
-	push = 1;
-        pop = 0;
-        #20;
-	push = 1;
-        pop = 0;
-        #40;
-
 	#20;
-	push = 1;
-        pop = 1;
-        #20;
 	push = 0;
         pop = 1;
+	#20;
+
+	push = 1;
+        pop = 0;
         #20;
 	push = 1;
-        pop = 1;
-
-        #50 $finish;
+        pop = 0;
+        #20;
+	push = 1;
+        pop = 0;
+	#20;
     end
-
 endmodule
-
