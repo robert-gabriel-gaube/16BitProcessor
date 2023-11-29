df4bcddceaf3b4963633c0693718e015d0851a7d
Cristian Crișan <107143484+ccrisan7@users.noreply.github.com>
2023-11-29 22:42:56 +0200

    Update INC_DEC_SP.v

--- SP/INC_DEC_SP.v
+++ SP/INC_DEC_SP.v
@@ -1,16 +1,9 @@
 module INC_DEC_SP(
     input wire pop,
     input wire [15:0] sp,
-    output reg [15:0] val
+    output [15:0] val
 );
 
-    always @(*) begin
-        if (pop == 1) begin
-            val = sp + 1;
-        end else begin
-            val = sp - 1;
-        end
-    end
+    assign val = (pop == 1) ? (sp + 1) : (sp - 1);
 
 endmodule
-
