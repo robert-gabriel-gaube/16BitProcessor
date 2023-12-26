a45c6135bce18412c790ee69b0f56b1ce1c80c73
robert-gabriel-gaube <robert-gabriel.gaube@student.upt.ro>
2023-12-26 12:32:15 +0200

    Alu done

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -1,28 +1,141 @@
-module ALU(input [15:0] A, B,   // Input operands
-           input [2:0] opcode,  // Operation code
-           output reg [15:0] out,// Output result
-           output reg Z, N, C, O // Flags
+module ALU(input [15:0] A, B, 
+           input [5:0] opcode,  
+           output reg [15:0] out,
+           output reg Z, N, C, O 
 );
 
-always @(A, B, opcode) begin
+reg [16:0] temp;
+reg [31:0] temp_mul;
+
+always @(*) begin
     case (opcode)
-        3'b000: begin // Addition
-            out = A + B;
+        6'h0A: begin // ADD
+            temp = A + B;
+            out = temp[15:0];
             Z = (out == 0);
-            N = (out < 0);
-            C = (out < A || out < B);
-            O = (A[15] == B[15] && A[15] != out[15]);
+            N = out[15];
+            C = ((A & B) != 0);
+            O = temp[16];
         end
 
-        3'b001: begin // Subtraction
-            out = A - B;
+        6'h0B, 6'h18: begin // SUB, CMP
+            temp = A - B;
+            out = temp[15:0];
             Z = (out == 0);
-            N = (out < 0);
-            C = (A < B);
-            O = (A[15] != B[15] && A[15] != out[15]);
+            N = out[15];
+            C = ((~A & B) != 0);
+            O = temp[16];
         end
 
-        // Add cases for multiplication and division
+        6'h0C: begin // LSR
+            out = A >> B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h0D: begin // LSL
+            out = A << B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h0E: begin // RSR
+            out = (A >> B) | (A << (16 - B));
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h0F: begin // RSL
+            out = (A << B) | (A >> (16 - B));
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h10: begin // MOV
+            out = B;
+            {Z, N, C, O} = 4'b0;
+        end
+
+        6'h11: begin // MUL
+            temp_mul = A * B;
+            out = temp_mul[15:0];
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = ((temp_mul >> 16) != 0);
+        end
+
+        6'h12: begin // DIV
+            out = A / B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h13: begin // MOD
+            out = A % B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h14, 6'h19: begin // AND, TST
+            out = A & B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h15: begin // OR
+            out = A | B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h16: begin // XOR
+            out = A ^ B;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h17: begin // NOT
+            out = ~A;
+            Z = (out == 0);
+            N = out[15];
+            C = 0;
+            O = 0;
+        end
+
+        6'h1A: begin // INC
+            out = A + 1;
+            Z = (out == 0);
+            N = out[15];
+            C = A[0];
+            O = (A == 16'hFFFF);
+        end
+
+        6'h1B: begin // DEC
+            out = A - 1;
+            Z = (out == 0);
+            N = out[15];
+            C = ~A[0];
+            O = (A == 16'h0000);
+        end
 
         default: begin
             // Handle unsupported opcode
--- ALU/ALU_tb.v
+++ ALU/ALU_tb.v
@@ -1,6 +1,6 @@
 module ALU_tb;
     reg [15:0] A, B;   // Input operand
-    reg [2:0] opcode;  // Operation code
+    reg [5:0] opcode;  // Operation code
     wire [15:0] out; // Output result
     wire Z, N, C, O;  // Flags
 
@@ -16,15 +16,157 @@ module ALU_tb;
     );
 
     initial begin
-        A = 10;
-        B = 10;
+        A = 16'hFFFF;
+        B = 16'h0001;
+        #100
+        A = 16'hFFFE;
+        B = 16'h0001;
     end
 
     initial begin 
-        opcode = 3'b0;
+        // 1. ADD
+        opcode = 6'b001010;
+        A = 16'hFFFF;
+        B = 16'h0001;
+        // out = 16'h0000; ZCO
+
         #100
-        opcode = 3'b1;
+
+        // 2. SUB
+        opcode = 6'b001011;
+        A = 16'hFFFE;
+        B = 16'h0001;
+        // out = 16'hFFFD; NC
+
         #100
-        opcode = 3'b1;
+
+        // 3. LSR
+        opcode = 6'b001100;
+        A = 16'h000F;
+        B = 16'h0004;
+        // out = 16'0000; Z
+
+        #100
+
+        // 4. LSL
+        opcode = 6'b001101;
+        A = 16'h3000;
+        B = 16'h0002;
+        // out = 16'hC000; N
+
+        #100
+
+        // 5. RSR
+        opcode = 6'b001110;
+        A = 16'h000B;
+        B = 16'h0002;
+        // out = 16'hC002; N
+
+        #100
+
+        // 6. RSL
+        opcode = 6'b001111;
+        A = 16'hB000;
+        B = 16'h0002;
+        // out = 16'hC002; N
+
+        #100
+
+        // 7. MOV
+        opcode = 6'b010000;
+        A = 16'h1234;
+        B = 16'h4321;
+        // out = 16'h4321;
+
+        #100
+
+        // 8. MUL
+        opcode = 6'b010001;
+        A = 16'h0002;
+        B = 16'h0004;
+        // out = 16'h0008;
+
+        #100
+
+        // 8. DIV
+        opcode = 6'b010010;
+        A = 16'h000A;
+        B = 16'h0002;
+        // out = 16'h0005;
+
+        #100
+
+        // 9. MOD
+        opcode = 6'b010011;
+        A = 16'h0009;
+        B = 16'h0002;
+        // out = 16'h0001;
+
+        #100
+
+        // 10. AND
+        opcode = 6'b010100;
+        A = 16'hAAAA;
+        B = 16'hAA22;
+        // out = 16'hAA22; N
+
+        #100
+
+        // 11. OR
+        opcode = 6'b010101;
+        A = 16'h8888;
+        B = 16'h2222;
+        // out = 16'hAAAA; N
+
+        #100
+
+        // 12. XOR
+        opcode = 6'b010110;
+        A = 16'hFFFF;
+        B = 16'h8888;
+        // out = 16'h7777;
+
+        #100
+
+        // 13. NOT
+        opcode = 6'b010111;
+        A = 16'hFF22;
+        B = 16'h0000;
+        // out = 16'h00DD;
+
+        #100
+
+        // 14. CMP
+        opcode = 6'b011000;
+        A = 16'hFFFE;
+        B = 16'h0001;
+        // out = 16'hFFFD; NC
+
+        #100
+
+        // 15. TST 
+        opcode = 6'b011001;
+        A = 16'hAAAA;
+        B = 16'hAA22;
+        // out = 16'hAA22; N
+
+        #100
+
+        // 16. INC
+        opcode = 6'b011010;
+        A = 16'h000F;
+        B = 16'h0000;
+        // out = 16'h0010; C
+
+        #100
+
+        // 17. DEC
+        opcode = 6'b011011;
+        A = 16'h000A;
+        B = 16'h0000;
+        // out = 16'h0009; C
+
+        #100
+        opcode = 6'b001101;
     end
 endmodule
